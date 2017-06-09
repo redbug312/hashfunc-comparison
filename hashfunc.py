e28c7de6da07f73bedc751546710dd3f8e97506a
RedBug312 <redbug312@gmail.com>
2017-06-10 00:59:05 +0800

    seperate functions from main in hashfunc.py; start using docstring

--- hashfunc.py
+++ hashfunc.py
@@ -1,25 +1,55 @@
 #! /usr/bin/python3
+""" Hash every inputs to generate a hashtable
+"""
+
 from functools import reduce
 
-djb2 = lambda bytelist: reduce(lambda prev, curr: (prev * 33 + curr) % (2 ** 32), bytelist, 5381)
+# hash functions: bytes -> int
+djb2 = lambda bytes_: reduce(lambda prev, curr: (prev * 33 + curr) % (2 ** 32), bytes_, 5381)
+
+def generate_hashtable(keys, hashfunc, bucket):
+    """ Generate the hashtable from given arguments
+    Args:
+        keys: string
+        hashfunc: bytes -> int
+        bucket: int
+    Returns:
+        hashtable: [[string]]
+    """
+    hashtable = [[] for _ in range(bucket)]
+    for key in keys:
+        byte = bytes(key.strip(), 'utf-8')
+        index = hashfunc(byte) % bucket
+        hashtable[index].append(key)
+    return hashtable
+
+def collision_stat(hashtable):
+    """ Statstic the (max, min, sum, mean) of collisions in hashtable
+    Args:
+        hashtable: {int: [string]}
+    Returns:
+        stat: (int)
+    """
+    return [len(bucket) for bucket in hashtable.values()]
+
 
 if __name__ == '__main__':
     import sys
     import json
     import getopt
-    import fileinput
 
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "b:", [])
+        opts, args = getopt.getopt(sys.argv[1:], "s:f:")
         options = dict(opts)
     except getopt.GetoptError:
         print('Invalid options')
         sys.exit(1)
 
-    buckets = int(options['-b']) if '-b' in options else 10
+    filename = options['-f']
+    size = int(options['-s']) if '-s' in options else 1600
 
-    result = [[] for _ in range(buckets)]
-    for line in fileinput.input():
-        byte = bytes(line, 'utf-8')
-        result[djb2(byte) % buckets].append(line)
-    print(json.dumps(result))
+    with open(filename) as f:
+        lines = f.readlines()
+
+    result = generate_hashtable(lines, djb2, size)
+    print(json.dumps(result, indent=4))
