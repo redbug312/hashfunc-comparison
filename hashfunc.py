a0af075b54d5b4fbba5fdf43cefa6f2cf7bd6e86
RedBug312 <redbug312@gmail.com>
2017-06-11 02:32:30 +0800

    very simple hash functions added

--- hashfunc.py
+++ hashfunc.py
@@ -2,15 +2,19 @@
 """ Hash every inputs to generate a hashtable
 """
 
+from operator import add, xor
 from functools import reduce
+import numpy
 
 # hash functions: bytes -> int
+sum8 = lambda bytes_: reduce(add, bytes_) % (2 ** 32)
+xor8 = lambda bytes_: reduce(xor, bytes_)
 djb2 = lambda bytes_: reduce(lambda prev, curr: (prev * 33 + curr) % (2 ** 32), bytes_, 5381)
 
 def generate_hashtable(keys, hashfunc, bucket):
     """ Generate the hashtable from given arguments
     Args:
-        keys: string
+        keys: [bytes] / [[int]]
         hashfunc: bytes -> int
         bucket: int
     Returns:
@@ -18,19 +22,21 @@ def generate_hashtable(keys, hashfunc, bucket):
     """
     hashtable = [[] for _ in range(bucket)]
     for key in keys:
-        byte = bytes(key.strip(), 'utf-8')
-        index = hashfunc(byte) % bucket
+        index = hashfunc(key) % bucket
         hashtable[index].append(key)
     return hashtable
 
+# def histogram_equilization(hashtable, bins):
+
 def collision_stat(hashtable):
-    """ Statstic the (max, min, sum, mean) of collisions in hashtable
+    """ Statstic the (average, standard deviation) of collisions in hashtable
     Args:
         hashtable: {int: [string]}
     Returns:
-        stat: (int)
+        stat: (int, int)
     """
-    return [len(bucket) for bucket in hashtable.values()]
+    collision = [len(bucket) for bucket in hashtable]
+    return (numpy.mean(collision), numpy.std(collision))
 
 
 if __name__ == '__main__':
@@ -49,7 +55,9 @@ if __name__ == '__main__':
     size = int(options['-s']) if '-s' in options else 1600
 
     with open(filename) as f:
-        lines = f.readlines()
+        lines = [bytes(line.strip(), 'utf-8') for line in f.readlines()]
+        # lines = [[int(line[0:3]), int(line[3:6]), int(line[6:9])] for line in f.readlines()]
+        # lines = [list(map(int, line.strip().split())) for line in f.readlines()]
 
-    result = generate_hashtable(lines, djb2, size)
+    result = generate_hashtable(lines, xor8, size)
     print(json.dumps(result, indent=4))
