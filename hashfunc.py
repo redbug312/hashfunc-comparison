de5df83bbed9c41bca8466c37adbfffe8c1e20b2
RedBug312 <redbug312@gmail.com>
2017-06-14 20:27:31 +0800

    histogram_equilized function implemented

--- hashfunc.py
+++ hashfunc.py
@@ -4,9 +4,16 @@
 
 from operator import add, xor
 from functools import reduce
+from collections import Counter
 import numpy
 
-# hash functions: bytes -> int
+# parse functions: string -> bytes=[int]
+bytelist = lambda line: list(map(int, line.strip().split()))
+raw_str  = lambda line: bytes(line.strip(), 'utf-8')
+sID_int  = lambda line: [int(line[0:3], 16), int(line[3:6], 16), int(line[6:9], 16)]
+sID_HE   = lambda line, he: [he(line[0:6]), int(line[6:9], 16)]
+
+# hash functions: bytes=[int] -> int
 sum8 = lambda bytes_: reduce(add, bytes_) % (2 ** 32)
 xor8 = lambda bytes_: reduce(xor, bytes_)
 djb2 = lambda bytes_: reduce(lambda prev, curr: (prev * 33 + curr) % (2 ** 32), bytes_, 5381)
@@ -18,7 +25,7 @@ def generate_hashtable(keys, hashfunc, bucket):
         hashfunc: bytes -> int
         bucket: int
     Returns:
-        hashtable: [[string]]
+        [[string]]
     """
     hashtable = [[] for _ in range(bucket)]
     for key in keys:
@@ -26,14 +33,28 @@ def generate_hashtable(keys, hashfunc, bucket):
         hashtable[index].append(key)
     return hashtable
 
-# def histogram_equilization(hashtable, bins):
+def histogram_equilized(keys, L):
+    """ Apply histogram equilization to the distribution of keys
+    Args:
+        keys: [string]
+        L: int
+    Returns:
+        string -> int
+    """
+    hist = Counter(keys)
+    labels, freqs = zip(*hist.items())
+    cdf = dict(zip(labels, numpy.cumsum(freqs)))
+
+    cdf_min = freqs[0]
+    MN = len(keys)
+    return lambda v: int(round((cdf[v] - cdf_min) / (MN - cdf_min) * (L - 1)))
 
 def collision_stat(hashtable):
     """ Statstic the (average, standard deviation) of collisions in hashtable
     Args:
         hashtable: {int: [string]}
     Returns:
-        stat: (int, int)
+        (int, int)
     """
     collision = [len(bucket) for bucket in hashtable]
     return (numpy.mean(collision), numpy.std(collision))
@@ -45,19 +66,18 @@ if __name__ == '__main__':
     import getopt
 
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "s:f:")
-        options = dict(opts)
+        opts, args = getopt.getopt(sys.argv[1:], "s:")
+        opts = dict(opts)
     except getopt.GetoptError:
-        print('Invalid options')
+        print('\nInvalid options')
         sys.exit(1)
 
-    filename = options['-f']
-    size = int(options['-s']) if '-s' in options else 1600
+    size = int(opts['-s']) if '-s' in opts else 256
+    with open(args[0]) as readfile:
+        lines = readfile.readlines()
 
-    with open(filename) as f:
-        lines = [bytes(line.strip(), 'utf-8') for line in f.readlines()]
-        # lines = [[int(line[0:3]), int(line[3:6]), int(line[6:9])] for line in f.readlines()]
-        # lines = [list(map(int, line.strip().split())) for line in f.readlines()]
+    h = histogram_equilized([line[0:6] for line in lines], size)
+    tokens = [sID_HE(line, h) for line in lines]
 
-    result = generate_hashtable(lines, xor8, size)
+    result = generate_hashtable(tokens, xor8, size)
     print(json.dumps(result, indent=4))
