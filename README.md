db7c62d6dfcbba0b84ff17dd97d00ec2e6299531
RedBug312 <redbug312@gmail.com>
2017-07-07 22:52:43 +0800

    getopts and update README

--- README.md
+++ README.md
@@ -1,14 +1,48 @@
 # hashfunc-comparison
 Test collision resistency non-cryptographic hash functions.
-This is a part of project for 2017 NTU CSIE Probability lecture.
+This is the experience part of project for 2017 NTU CSIE Probability lecture.
 
-## How to use
-In your terminal:
+![](https://s3.amazonaws.com/media-p.slid.es/uploads/396189/images/3908294/Figure_1-14.png)
+
+## Presentation part
+* slide: [XOR8 hash function](https://slides.com/redbug312/xor8-hash-function)
+
+We uses some optimizations to have **xor8** hash behaves as good as **djb2**, even in less time.
+
+```shell
+$ ./hashfunc.py --hash xor8 data/studentID | ./barchart.py -g
+$ ./hashfunc.py --hash djb2 data/studentID | ./barchart.py -g
+
+# Optimization 1
+$ ./hashfunc.py --hash xor8 --size 64 data/studentID | ./barchart.py -g
+
+# Optimization 2
+$ ./hashfunc.py --hash xor8 --size 64 --regroup data/studentID | ./tilechart.py -g
+
+# Optimization 3
+$ ./hashfunc.py --hash xor8 --size 64 --regroup --hist-eq=0,6 data/studentID | ./tilechart.py -g
 ```
-$ ./hashfunc.py -f data/studentID | ./tilechart.py
-```
+
+Notice that the data is slightly transforned due to privacy, thus the result would behaves different from the slide above. But there’s the same effect on each optimization.
+
+## Structure
+The program can also be used to analyze other hash functions. `hashfunc.py` will read lines in file given as first argument, printing out a hashtable. `barchart.py` and `tilechart.py` will read the hashtable and show the responding chart.
+
+1. Options in `hashfunc.py`
+    * `-h`, `--hash`: specify the hash function, **sum8, xor8, djb2** is implemented
+    * `-s`, `--size`: specify the bucket number in the hashtable
+    * `-r`, `--regroup`: specify whether to use shift folding, suitable only for `data/studentID` now
+    * `-e`, `--hist-eq`: specify the subscription for each line to perform histogram equilization
+2. Options in `barchart.py`, `tilechart.py`
+    * `-g`: specify using pyplot to show the chart, instead of terminal
 
 And it will show the distribution of djb2 hashing result from every line in the file `studentID`
 
 ## Environments
 * Python3 with numpy, matplotpy installed
+
+## References
+* [HASHING](https://www.csie.ntu.edu.tw/~hsinmu/courses/_media/dsa_12spring/hashing.pdf)
+* [Which hashing algorithm is best for uniqueness and speed?](https://softwareengineering.stackexchange.com/questions/49550/which-hashing-algorithm-is-best-for-uniqueness-and-speed) | StackExchange
+* [Gaussian Distribution + Hash Tables](https://stackoverflow.com/questions/4324490/gaussian-distribution-hash-tables?rq=1) | StackOverflow
+* [Why are 5381 and 33 so important in the djb2 algorithm?](https://stackoverflow.com/questions/1579721/why-are-5381-and-33-so-important-in-the-djb2-algorithm)
--- hashfunc.py
+++ hashfunc.py
@@ -1,6 +1,6 @@
 #! /usr/bin/python3
-""" It hashes every inputed lines to generate a hashtable
-"""
+''' It hashes every inputed lines to generate a hashtable
+'''
 
 from operator import add, xor
 from functools import reduce
@@ -19,13 +19,13 @@ djb2 = lambda bytes_: reduce(lambda prev, curr: (prev * 33 + curr) % (2 ** 32), 
 
 
 def histogram_equilized(keys, L):
-    """ Apply histogram equilization to the distribution of keys
+    ''' Apply histogram equilization to the distribution of keys
     Args:
         keys as `[string]`
         upper bound of range as `int` (lower bound as 0)
     Returns:
         mapping as `string -> int`
-    """
+    '''
     hist = OrderedDict(Counter(keys))
     labels, freqs = zip(*hist.items())
     cdf = dict(zip(labels, numpy.cumsum(freqs)))
@@ -35,8 +35,8 @@ def histogram_equilized(keys, L):
     HE = lambda v: int(round((cdf[v] - cdf_min) / (MN - cdf_min) * (L - 1)))
     return HE
 
-def parse_tokens(keys, parsefunc, HEsub=None):
-    """ Attach the parsed tokens to each key
+def parse_tokens(keys, parsefunc_, HEsub=None):
+    ''' Attach the parsed tokens to each key
     Args:
         keys as `[string]`
         parsefunc to parse key as `string -> [int]`
@@ -44,30 +44,31 @@ def parse_tokens(keys, parsefunc, HEsub=None):
             as `(start, end) | None`, None for turning off HE
     Returns:
         keys attached with tokens as `[(string, [int])]`
-    """
+    '''
     if HEsub is not None:
         substr = lambda key: key[HEsub[0]:HEsub[1]]
         HEfunc = histogram_equilized([substr(key) for key in keys], size)
         HEval = lambda key: HEfunc(substr(key))
-        tokens = [parsefunc(line) + [HEval(line)] for line in lines]
+        tokens = [parsefunc_(line) + [HEval(line)] for line in lines]
     else:
-        tokens = [parsefunc(line) for line in lines]
+        tokens = [parsefunc_(line) for line in lines]
     return zip(lines, tokens)
 
 def generate_hashtable(keys, hash_, bucket):
-    """ Generate the hashtable from given arguments
+    ''' Generate the hashtable from given arguments
     Args:
         keys as `[(string, [int])]`
         hash function as `bytes -> int`
         total bucket number as `int`
     Returns:
         [[string]]
-    """
+    '''
     hashtable = [[] for _ in range(bucket)]
     for key in keys:
         index = hash_(key[1]) % bucket
         hashtable[index].append(key[0])
     return hashtable
+
 
 if __name__ == '__main__':
     import sys
@@ -75,22 +76,31 @@ if __name__ == '__main__':
     import getopt
 
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "", ["size=", "regroup", "hist-eq="])
-        opts = dict(opts)
+        opts, args = getopt.getopt(sys.argv[1:], \
+                     'h:s:re:', ['hash=', 'size=', 'regroup', 'hist-eq='])
     except getopt.GetoptError:
         print('\nInvalid options')
         sys.exit(1)
 
-    print(opts)
+    hashfunc = xor8
+    size = 60
+    parsefunc = raw_str
+    histeq_sub = None
 
-    size = int(opts['--size']) if '--size' in opts else 256
-    hashfunc = sID_int if '--regroup' in opts else raw_str
-    histeq_sub = tuple(map(int, opts['--hist-eq'].split(','))) if '--hist-eq' in opts else None
+    # print(dict(opts))
+    for opt, val in opts:
+        if opt in ('-h', '--hash') and val in ('sum8', 'xor8', 'djb2'):
+            hashfunc = {'sum8': sum8, 'xor8': xor8, 'djb2': djb2}[val]
+        elif opt in ('-s', '--size'):
+            size = int(val)
+        elif opt in ('-r', '--regroup'):
+            parsefunc = sID_int
+        elif opt in ('-e', '--hist-eq'):
+            histeq_sub = tuple(map(int, val.split(',')))
 
     with open(args[0]) as readfile:
         lines = [readline.strip() for readline in readfile.readlines()]
-
-    parsed_lines = parse_tokens(lines, hashfunc, histeq_sub)
-    result = generate_hashtable(parsed_lines, xor8, size)
+    parsed_lines = parse_tokens(lines, parsefunc, histeq_sub)
+    result = generate_hashtable(parsed_lines, hashfunc, size)
 
     print(json.dumps(result, indent=4))
